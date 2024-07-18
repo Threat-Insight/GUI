9c5f003e17ccfe0add1da1f7096c107d3671d35e
secfreaky <singhabhinav3290@gmail.com>
2024-07-19 00:14:20 +0530

    added teams.jsx

---
+++ src/components/Teams.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "../components/Navbar"
+
+export default function Team(){
+    const links = [
+        {
+          title: "Home",
+          redirect: "/",
+        },
+        {
+          title: "About",
+          redirect: "/about",
+        },
+        {
+          title: "Documentation",
+          redirect: "/documentation",
+        },
+        {
+          title: "Contribute",
+          redirect: "https://github.com/syncattacker/ProjectSafeLink",
+        },
+        // {
+        //   title: "Team",
+        //   redirect: "/meet-the-team",
+        // },
+      ];
+    return <Navbar links={links}/>
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Scan from "./pages/Scan";
-import Card from "./components/Card";
+import Teams from "./components/Teams"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,7 +11,7 @@ root.render(
     <Routes>
       <Route exact path="/" element={<App />} />
       <Route exact path="/scan" element={<Scan />} />
-      <Route exact path="/cards" element={<Card />} />
+      <Route exact path="/meet-the-team" element={<Teams />} />
     </Routes>
   </Router>
 );
