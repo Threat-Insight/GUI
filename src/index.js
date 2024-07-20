06f82646a14c58b6f18f615bc5cc86d6028b401d
syncattacker <cybereyerahul@gmail.com>
2024-07-20 20:42:29 +0530

    Changes Updated and Fixes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Scan from "./pages/Scan";
 import Teams from "./components/MeetOurTeam";
-import Documentation from "./components/Documentation";
+import Documentation from "./pages/Documentation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
